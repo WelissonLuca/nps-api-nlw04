f5bb99eb189843faab0de90a703c19d7b13b6ff3
Welisson Luca <welissonluca17@gmail.com>
2021-02-23 18:11:15 -0300

    feat: create usersRepository

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,9 +1,18 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { User } from '../models/User';
 
 class UserController {
     async create(req: Request, res: Response) {
-        const body = req.body;
-        console.log(body)
+        const {name, email} = req.body;
+        
+        const usersRepository = getRepository(User);
+        const user = usersRepository.create({
+            name, email
+        })
+
+        await usersRepository.save(user);
+        
         return res.send()
     }
 }
