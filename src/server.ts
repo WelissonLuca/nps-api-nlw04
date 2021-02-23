eb138760e9a7afb661a73b5951c3cd62c53d23bb
Welisson Luca <welissonluca17@gmail.com>
2021-02-22 21:14:21 -0300

    feat: ts-node-dev initial configuration

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,12 @@ import express from 'express';
 
 const app = express();
 
+
+app.get('/', (req, res) => {
+    return res.json({message: "Hello World - NLW04"})
+})
+
+app.post('/', (req, res) => {
+    return res.json({message: 'Os dados foram salvos com sucesso'})
+})
 app.listen(3333, () => console.log('server is running'))
