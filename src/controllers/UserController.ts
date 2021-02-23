952ffbefe79b44defe587e574871714ac8a57630
Welisson Luca <welissonluca17@gmail.com>
2021-02-23 16:50:39 -0300

    feat: Routes.ts Create

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,7 +2,9 @@ import { Request, Response } from 'express';
 
 class UserController {
     async create(req: Request, res: Response) {
-        
+        const body = req.body;
+        console.log(body)
+        return res.send()
     }
 }
 
