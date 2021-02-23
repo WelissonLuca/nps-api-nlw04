5622b46434ba01d4b5f8b472c8fa1d7b1b5a6ac0
Welisson Luca <welissonluca17@gmail.com>
2021-02-23 18:21:08 -0300

    feat: create userAlreadyExists

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
+import { FindRelationsNotFoundError, getRepository } from 'typeorm';
 import { User } from '../models/User';
 
 class UserController {
@@ -7,13 +7,24 @@ class UserController {
         const {name, email} = req.body;
         
         const usersRepository = getRepository(User);
+        
+        
+        
+        const userAlreadyExists = await usersRepository.findOne({
+            email
+        });
+        if (userAlreadyExists) {
+            return res.status(400).json({
+                error: "User already exists !"
+            })
+        }
         const user = usersRepository.create({
             name, email
         })
-
+            ;
         await usersRepository.save(user);
-        
-        return res.send()
+
+        return res.json(user);
     }
 }
 
