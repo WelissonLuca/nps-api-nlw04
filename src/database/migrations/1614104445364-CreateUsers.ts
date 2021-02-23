24fe1460ec11022f6f0e8636259c8253ef012a95
Welisson Luca <welissonluca17@gmail.com>
2021-02-23 15:21:22 -0300

    feat: migrationsDir configured

---
+++ src/database/migrations/1614104445364-CreateUsers.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUsers1614104445364 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
